79ea7f5526e2c1e12b6bb622e52402169bea1f93
Chanukaa2002 <mchanuka72@gmail.com>
2024-11-02 20:13:22 +0530

    twitter_clone backend

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -15,7 +15,7 @@ export const protectRoute = async (req, res, next) => {
 
     const user = await User.findById(decode.id).select("-password");
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "Please login first" });
     }
 
     req.user = user;
